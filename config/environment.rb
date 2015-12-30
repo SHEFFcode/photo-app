6dece7ba2f9931c85183146d3d00c72c163c1de4
Yuriy Shefer <sheff@sheffmachine.com>
2015-12-30 15:58:35 -0800

    update the environment information to attempt to fix production confirmation email

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-	:address => 'smtp.sendgrid.net', 
+	:address => 'smtp.sendgrid.net',
 	:port => '587',
 	:authentication => :plain,
-	:username => ENV['SENDGRID_USERNAME'],
+	:user_name => ENV['SENDGRID_USERNAME'],
 	:password => ENV['SENDGRID_PASSWORD'],
 	:domain => 'heroku.com',
 	:enable_starttls_auto => true
