3563a8f62aa808dc19d5aa73bcbf70584f97418d
Yuriy Shefer <sheff@sheffmachine.com>
2015-12-31 16:40:29 -0800

    add stipe credit card processing for users

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+Rails.configuration.stripe = {
+	:publishable_key => 'pk_test_rNEI8ZVwGE8QjSEw8LazFPbY',
+	:secret_key => 'sk_test_cFULQuaQe0muWlwa64P260CG'
+}
+
+Stripe.api_key = 'sk_test_cFULQuaQe0muWlwa64P260CG'
